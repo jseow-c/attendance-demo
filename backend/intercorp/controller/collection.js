2577cada2b32d73633516d771eaab8c8625bc16b
Jun Liang Seow (jseow) <jseow@cisco.com>
2020-01-02 18:44:36 +0800

    added in proxy using https-proxy-agent into collectionController. Debug

--- backend/intercorp/controller/collection.js
+++ backend/intercorp/controller/collection.js
@@ -1,12 +1,18 @@
 const axios = require("axios");
 const options = require("../options");
+const HttpsProxyAgent = require("https-proxy-agent");
 
 const { baseUrl, headers } = options;
 
+const IntercorpOptions = { headers };
+if (process.env.PROXY) {
+  IntercorpOptions.proxy = false;
+  IntercorpOptions.httpsAgent = new HttpsProxyAgent(process.env.PROXY);
+}
+
 exports.list = async (req, res) => {
   const url = `${baseUrl}/collections`;
-  const options = { headers };
-  const collections = await axios.get(url, options);
+  const collections = await axios.get(url, IntercorpOptions);
   return res.json(collections.data.response);
 };
 
@@ -14,17 +20,14 @@ exports.create = async (req, res) => {
   const url = `${baseUrl}/collection`;
   const name = req.body.name;
   const id = `${name.replace(/\s+/g, "")}Id`;
-  const options = { headers };
   const postData = { name, id };
-  const response = await axios.post(url, postData, options);
+  const response = await axios.post(url, postData, IntercorpOptions);
   return res.json(response.data.response);
 };
 
 exports.delete = async (req, res) => {
   const id = req.body.id;
   const url = `${baseUrl}/collection/${id}`;
-  const options = { headers };
-  console.log(url, options);
-  await axios.delete(url, options);
+  await axios.delete(url, IntercorpOptions);
   return res.json({});
 };
